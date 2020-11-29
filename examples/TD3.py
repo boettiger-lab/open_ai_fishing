4b9d71d5da024e302d4fd84b56ff9c3c079a5d26
Ardor-Wu <70617529+Ardor-Wu@users.noreply.github.com>
2020-11-29 10:41:06 +0800

    PPO-test with 20000 total time steps instead of 200000 and include loading model directly branch in TD3-test

--- examples/TD3.py
+++ examples/TD3.py
@@ -3,7 +3,7 @@ import gym_fishing
 from stable_baselines3 import TD3
 from stable_baselines3.common.evaluation import evaluate_policy
 
-env = gym.make('fishing-v2', C = 0.01)
+env = gym.make('fishing-v2', C=0.01)
 model = TD3('MlpPolicy', env, verbose=0)
 model.learn(total_timesteps=100000)
 
@@ -16,5 +16,3 @@ mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=50)
 print("mean reward:", mean_reward, "std:", std_reward)
 
 model.save("results/td3-tipping")
-
-
--- tests/TD3-test.py
+++ tests/TD3-test.py
@@ -4,13 +4,15 @@ import gym_fishing
 from stable_baselines3 import TD3
 from stable_baselines3.common.env_checker import check_env
 
-
 env = gym.make('fishing-v1')
 check_env(env)
 
-
-model = TD3('MlpPolicy', env, verbose=1)
-model.learn(total_timesteps=200)
+load = False
+if load:
+    model = TD3.load("td3")
+else:
+    model = TD3('MlpPolicy', env, verbose=1)
+    model.learn(total_timesteps=200)
 
 ## Simulate a run with the trained model, visualize result
 df = env.simulate(model)
@@ -18,10 +20,11 @@ env.plot(df, "td3.png")
 
 ## Evaluate model
 from stable_baselines3.common.evaluation import evaluate_policy
-mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=5)
+
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
 print("mean reward:", mean_reward, "std:", std_reward)
 
-
 ## Save and reload the model
-model.save("td3")
-model = TD3.load("td3")
+if not load:
+    model.save("td3")
+    model = TD3.load("td3")
