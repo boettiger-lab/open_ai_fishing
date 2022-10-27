f1ac9943a144895f5aadb0cb104c12b2123c72be
Felipe Montealegre Mora <felimomouni@gmail.com>
2022-10-27 15:39:10 -0600

    typo

--- gym_fishing/envs/forage_VVH_env.py
+++ gym_fishing/envs/forage_VVH_env.py
@@ -127,10 +127,10 @@ class forageVVH(gym.Env):
             1.0
         )  # later on used to 'turn off' model complexity
         self.beta = np.float32(0.5)
-        self.f = np.float(0.5)
+        self.f = np.float32(0.5)
         self.D = np.float32(1.0)  # no discrepancy for now!
         self.V0 = np.float32(self.K["V1"] / 2)
-        self.dH = np.float(0.3)
+        self.dH = np.float32(0.3)
 
     def bound_popspace(self) -> None:
         """
