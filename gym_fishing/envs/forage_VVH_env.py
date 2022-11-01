b1ab17250856ace72d14a229feceddfaf0506a27
Felipe Montealegre Mora <felimomouni@gmail.com>
2022-11-01 13:26:07 -0600

    now dynamical sigmas are 0, alpha is 0.005

--- gym_fishing/envs/forage_VVH_env.py
+++ gym_fishing/envs/forage_VVH_env.py
@@ -91,7 +91,7 @@ class forageVVH(gym.Env):
         }
 
         self.alpha = np.float32(
-            0.001
+            0.005
         )  # later on used to 'turn off' model complexity
         
         """
